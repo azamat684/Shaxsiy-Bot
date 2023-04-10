efe22a27d218f2814c828a71ebb43c5ebae2b19b
Azamat Dosmukhambetov <122520658+azamat684@users.noreply.github.com>
2023-04-11 03:26:34 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -4,6 +4,7 @@ from environs import Env
 env = Env()
 env.read_env()
 
+
 # .env fayl ichidan quyidagilarni o'qiymiz
 BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
