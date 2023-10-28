98b4f8adad2b77dce07989c83d703c235fb2d9d9
Azamat Dosmukhambetov <122520658+azamat684@users.noreply.github.com>
2023-10-28 23:28:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ async def on_startup(dispatcher):
 
     # Ma'lumotlar bazasini yaratamiz:
     try:
-        db.create_table_game()
+        db.create_table_users()
     except Exception as err:
         print(err)
 
