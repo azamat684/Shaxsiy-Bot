1f482b04d81c359aff07b2a244454a1db604a453
Azamat Dosmukhambetov <122520658+azamat684@users.noreply.github.com>
2024-01-28 21:09:05 +0500

    Update help.py

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -6,7 +6,6 @@ from filters.IsPrivate import IsPrivate
 
 @dp.message_handler(IsPrivate(),CommandHelp(),state="*")
 async def bot_help(message: types.Message,state: FSMContext):
-        await state.finish()
         bot_info = await bot.get_me()
         text = (f"Bu bot nima qila oladi?\n\n"
                 f"Ushbu bot orqali siz \n\n▪️ <b>Youtube</b>,<b>Instagram</b>,<b>Tik Tok</b> ishtimoiy tarmoqlaridan <b>video rasmlarni</b> yuklab olishingiz\n\n<b>▪️ Ob-Havo</b> malumotlari haqida bilib olishingiz\n\n▪️ To'g'ridan to'g'ri <b>google</b> dan biror narsa xaqida malumot so'rashingiz \n\n▪️ <b>Matnli</b> xabarlarni <b>Ovozli</b> xabarga aylatirishingiz\nva\n▪️ <b>Tarjimondan</b> foydalanishingiz mumkin 😊\n\nBotni Kamandalari\n\n"
@@ -16,3 +15,4 @@ async def bot_help(message: types.Message,state: FSMContext):
                 f"Bu bot Inline rejimdaham ishlaydi va bot usernameni olasizda va biror chatga kirib bot usernameni qo'yib keyin izidan biror bir narsa qidirasiz va bot sizga youtubedan video topib beradi 🤩\n\nTushinmagan bo'lsangiz misol: @{bot_info.username} Biror narsa")
         
         await message.answer(text,parse_mode="HTML")
+        await state.finish()
